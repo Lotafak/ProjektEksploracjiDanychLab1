36de6e7dfd01c93f88aae0a7b49d5d7ed926066f
patrykk <patrykk.93@gmail.com>
2016-11-14 10:37:57 +0100

    Deviation isntead od variation in Feature. Writing statistics to file. Standarization and normalization.

--- ProjektEksploracjiDanychLab1/Feature.cs
+++ ProjektEksploracjiDanychLab1/Feature.cs
@@ -9,26 +9,42 @@ namespace ProjektEksploracjiDanychLab1
 {
     class Feature
     {
+        public Feature Clone()
+        {
+            return new Feature()
+            {
+                Count = Count,
+                Sum = Sum,
+                Values = new List<double>(Values)
+            };
+        }
+
         public int Count { get; set; }
         public double Sum { get; set; }
         public double Mean { get; set; }
-        public double Variance { get; set; }
+        public double Deviation { get; set; }
         public SortedDictionary<double, int> UniqueValues { get; set; } = new SortedDictionary<double, int>();
         public List<double> Values { get; set; } = new List<double>();
 
         public void CalculateMean()
         {
+            Sum = 0;
+            foreach (var value in Values)
+            {
+                Sum += value;
+            }
             Mean = Sum/Count;
         }
 
-        public void CalculateVariance()
+        public void CalculateStandardDeviation()
         {
+            Deviation = 0;
             foreach (var value in Values)
             {
-                Variance += Math.Pow(value - Mean, 2);
+                Deviation += Math.Pow(value - Mean, 2);
             }
 
-            Variance = Variance/Count;
+            Deviation = Math.Sqrt(Deviation/Count);
         }
     }
 }
--- ProjektEksploracjiDanychLab1/Program.cs
+++ ProjektEksploracjiDanychLab1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 
 namespace ProjektEksploracjiDanychLab1
 {
@@ -10,15 +12,24 @@ namespace ProjektEksploracjiDanychLab1
     {
         static void Main()
         {
-            List<string[]> examples = new List<string[]>();
+            var examples = new List<double[]>();
+            var timer = new Stopwatch();
+            timer.Start();
 
-            using ( StreamReader sr = new StreamReader("X_train.csv") )
+            using ( var sr = new StreamReader("X_train.csv") )
             {
                 while ( !sr.EndOfStream )
                 {
                     var readLine = sr.ReadLine();
-                    if ( readLine != null )
-                        examples.Add(readLine.Split(','));
+                    if (readLine == null) continue;
+
+                    var temp = readLine.Split(',');
+                    var doubleTemp = new double[temp.Length];
+                    for(var i = 0; i < temp.Length; i++ )
+                    {
+                        doubleTemp[i] = double.Parse(temp[i], CultureInfo.InvariantCulture);
+                    }
+                    examples.Add(doubleTemp);
                 }
             }
 
@@ -33,20 +44,20 @@ namespace ProjektEksploracjiDanychLab1
                 features.Add(new Feature());
             }
 
+            var t0 = timer.ElapsedMilliseconds;
+
             foreach ( var example in examples )
             {
                 for ( var i = 0; i < example.Length; i++ )
                 {
-                    var featureValue = Convert.ToDouble(example[i], CultureInfo.InvariantCulture);
+                    var featureValue = example[i];
                     var feature = features[i];
-                    
 
-                    if ( featureValue > 0 )
+                    if ( Math.Abs(featureValue) > 0.00000000001)
                     {
                         nonZeroFeatures++;
                     }
-
-                    feature.Sum += featureValue;
+                    
                     feature.Count++;
 
                     var uniqueValues = feature.UniqueValues;
@@ -56,23 +67,133 @@ namespace ProjektEksploracjiDanychLab1
                         uniqueValues.Add(featureValue, 1);
 
                     feature.Values.Add(featureValue);
-
                 }
             }
 
-            StreamWriter sw = new StreamWriter("data.txt");
+            var sw = new StreamWriter("datax.txt");
+
+            var dataRepresentationTime = timer.ElapsedMilliseconds - t0;
+
+            sw.WriteLine($"Time of loading data to memory: {dataRepresentationTime}");
+            
             var drawer = new Drawer();
-            var name = 0;
+
+            var globalRawMax = double.MinValue;
+            var globalRawMin = double.MaxValue;
             foreach ( var feature in features )
             {
                 feature.CalculateMean();
-                feature.CalculateVariance();
-                drawer.DrawChart(name++.ToString(), feature.UniqueValues);
-                sw.WriteLine($"{name} \nMean: {feature.Mean}\nVariance: {feature.Variance}\nUnique values: {feature.UniqueValues.Count}");
-                sw.WriteLine();
+                feature.CalculateStandardDeviation();
+                var max = feature.Values.Max();
+                var min = feature.Values.Min();
+                if ( globalRawMax < max )
+                    globalRawMax = max;
+
+                if ( globalRawMin > min )
+                    globalRawMin = min;
+                //drawer.DrawChart(name++.ToString(), feature.UniqueValues);
+                //sw.WriteLine($"{name} \nMean: {feature.Mean}\nVariance: {feature.Variance}\nUnique values: {feature.UniqueValues.Count}");
+                //sw.WriteLine();
             }
 
-            Console.WriteLine($"Liczba niezerowych cech: {nonZeroFeatures:N0}");
+            sw.WriteLine($"Statistics calculation: {timer.ElapsedMilliseconds - dataRepresentationTime}");
+            var featuresCopy = features.Select(feature => feature.Clone()).ToList();
+
+            sw.WriteLine("Raw");
+            sw.WriteLine($"Max: {features[0].Values.Max()}");
+            sw.WriteLine($"Min: {features[0].Values.Min()}");
+            sw.WriteLine($"Global max: {globalRawMax}");
+            sw.WriteLine($"Global min: {globalRawMin}");
+            sw.WriteLine($"Mean: {features[0].Mean}");
+            sw.WriteLine($"Deviation: {features[0].Deviation}");
+            sw.WriteLine();
+
+            var startStandarizing = timer.ElapsedMilliseconds;
+
+            var x = timer.ElapsedMilliseconds;
+            var unused = features[200].Deviation;
+            var y = timer.ElapsedMilliseconds - x;
+
+            var globalStandarizedMax = double.MinValue;
+            var globalStandarizedMin = double.MaxValue;
+            foreach (var feature in features)
+            {
+                for ( var i = 0; i < feature.Values.Count; i++ )
+                {
+                    feature.Values[i] = ( feature.Values[i] - feature.Mean ) / feature.Deviation;
+                }
+                var max = feature.Values.Max();
+                var min = feature.Values.Min();
+                if (globalStandarizedMax < max)
+                    globalStandarizedMax = max;
+
+                if (globalStandarizedMin > min)
+                    globalStandarizedMin = min;
+            }
+            
+            features[0].CalculateMean();
+            features[0].CalculateStandardDeviation();
+
+            sw.WriteLine("Standarized");
+            sw.WriteLine($"Max: {features[0].Values.Max()}");
+            sw.WriteLine($"Min: {features[0].Values.Min()}");
+            sw.WriteLine($"Global max: {globalStandarizedMax}");
+            sw.WriteLine($"Global min: {globalStandarizedMin}");
+            sw.WriteLine($"Mean: {features[0].Mean}");
+            sw.WriteLine($"Deviation: {features[0].Deviation}");
+            sw.WriteLine();
+
+
+            var globalNormalizedMax = double.MinValue;
+            var globalNormalizedMin = double.MaxValue;
+            foreach (var feature in featuresCopy)
+            {
+                var max = feature.Values.Max();
+                var min = feature.Values.Min();
+                for ( var i = 0; i < feature.Values.Count; i++ )
+                {
+                    feature.Values[i] = ( feature.Values[i] - min ) / ( max - min);
+                }
+                max = feature.Values.Max();
+                min = feature.Values.Min();
+
+                if ( globalNormalizedMax < max )
+                    globalNormalizedMax = max;
+
+                if ( globalNormalizedMin > min )
+                    globalNormalizedMin = min;
+            }
+
+            featuresCopy[0].CalculateMean();
+            featuresCopy[0].CalculateStandardDeviation();
+            
+            sw.WriteLine("Normalized");
+            sw.WriteLine($"Max: {featuresCopy[0].Values.Max()}");
+            sw.WriteLine($"Min: {featuresCopy[0].Values.Min()}");
+            sw.WriteLine($"Global max: {globalNormalizedMax}");
+            sw.WriteLine($"Global min: {globalNormalizedMin}");
+            sw.WriteLine($"Mean: {featuresCopy[0].Mean}");
+            sw.WriteLine($"Deviation: {featuresCopy[0].Deviation}");
+            
+            sw.WriteLine($"Time of data standarization/normalization: {timer.ElapsedMilliseconds - startStandarizing}");
+
+            var classList = new List<int>();
+            using ( var sr = new StreamReader("y_train.csv") )
+            {
+                while ( !sr.EndOfStream )
+                {
+                    var readline = sr.ReadLine();
+                    if ( readline == null ) continue;
+
+                    classList.Add(int.Parse(readline));
+                }
+            }
+            var ones = classList.FindAll(c => c == 1);
+            sw.WriteLine($"Class \"1\": {ones.Count}");
+            sw.WriteLine($"Class \"0\": {classList.Count - ones.Count}");
+            sw.Close();
+
+            Console.WriteLine($"Liczba niezerowych cech: {nonZeroFeatures:N0} ({(double)nonZeroFeatures / ( examples.Count * examples[0].Length ) * 100:N2}%)");
 
             Console.ReadLine();
         }
